ca22f230ae52bc787b727299cdd6b50cd4bd99dd
csvidit <63170797+csvidit@users.noreply.github.com>
2024-07-13 21:44:58 -0400

    fix link typos

--- src/utils/urls/demo-urls.ts
+++ src/utils/urls/demo-urls.ts
@@ -1,7 +1,7 @@
 export const BENTO_PORTFOLIO_DEMO_URL =
-  "https://bento-portfolio.demos.snippetopa.xyz";
-export const BENTO_BIO_DEMO_URL = "https://bento-bio.demos.snippetopa.xyz";
-export const SWISS_BIO_DEMO_URL = "https://swiss-bio.demos.snippetopa.xyz";
+  "https://bento-portfolio.demos.snippetopia.xyz";
+export const BENTO_BIO_DEMO_URL = "https://bento-bio.demos.snippetopia.xyz";
+export const SWISS_BIO_DEMO_URL = "https://swiss-bio.demos.snippetopia.xyz";
 export const INVERNO_PORTFOLIO_DEMO_URL =
-  "https://inverno-portfolio.demos.snippetopa.xyz";
-export const BRAT_BIO_DEMO_URL = "https://brat-bio.demos.snippetopa.xyz";
+  "https://inverno-portfolio.demos.snippetopia.xyz";
+export const BRAT_BIO_DEMO_URL = "https://brat-bio.demos.snippetopia.xyz";
