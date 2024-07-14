49e01a3f2f71da6d814dcf9478b43a961c592e9a
csvidit <63170797+csvidit@users.noreply.github.com>
2024-07-13 21:00:23 -0400

    add url constants

---
+++ src/utils/urls/demo-urls.ts
@@ -0,0 +1,6 @@
+export const BENTO_PORTFOLIO_DEMO_URL =
+  "https://bento-portfolio.demos.snippetopa.xyz";
+export const BENTO_BIO_DEMO_URL = "https://bento-bio.demos.snippetopa.xyz";
+export const SWISS_BIO_DEMO_URL = "https://swiss-bio.demos.snippetopa.xyz";
+export const INVERNO_PORTFOLIO_DEMO_URL =
+  "https://inverno-portfolio.demos.snippetopa.xyz";
---
+++ src/utils/urls/github-urls.ts
@@ -0,0 +1,7 @@
+export const BENTO_PORTFOLIO_GITHUB_URL =
+  "https://github.com/csvidit/bento-portfolio-concept";
+export const BENTO_BIO_GITHUB_URL =
+  "https://github.com/csvidit/bento-bio-concept";
+export const SWISS_BIO_GITHUB_URL = "https://github.com/csvidit/bio-swiss";
+export const INVERNO_PORTFOLIO_GITHUB_URL =
+  "https://github.com/csvidit/inverno-portfolio";
