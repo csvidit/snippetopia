fca6554fd2cbf21cfe3d362df13c6b8faadb52d9
csvidit <63170797+csvidit@users.noreply.github.com>
2025-02-05 23:55:40 +0530

    (src/app/pop/page.tsx) remove unused imports

--- src/app/pop/page.tsx
+++ src/app/pop/page.tsx
@@ -1,5 +1,3 @@
-import InvernoIntro from "@/components/Inverno/InvernoIntro";
-import InvernoList from "@/components/Inverno/InvernoList";
 import MainContent from "@/components/MainContent";
 import PopIntro from "@/components/Pop/PopIntro";
 
